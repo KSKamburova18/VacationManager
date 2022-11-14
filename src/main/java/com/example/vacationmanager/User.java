79e25fe422ce0f6810236a05d62f9896441db985
KSKamburova18 <KSKamburova18@codingburgas.bg>
2022-11-14 11:42:38 +0200

    Initial commit

---
+++ src/main/java/com/example/vacationmanager/User.java
@@ -0,0 +1,9 @@
+package com.example.vacationmanager;
+
+public class User {
+    private String username;
+    private int password;
+    private String name;
+    private String role;
+    private String team;
+}
